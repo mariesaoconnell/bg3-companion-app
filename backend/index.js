f2f8598a4e2ac2b406c1f5a849e1fc8b6e1e96f2
mariesaoconnell <mariesa.oconnell@gmail.com>
2023-10-05 22:24:15 -0400

    Start front end

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
+
 const port = 3000;
 
 // IMPORT ROUTES
@@ -9,7 +11,11 @@ companionApprovalsRouter = require('./routes/companionApprovals');
 createApprovalRouter = require('./routes/createApproval');
 deleteApproval = require('./routes/deleteApproval')
 
+var corsOptions = {
+	origin: 'http://localhost:3001',
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(
 	express.urlencoded({
@@ -18,7 +24,7 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-	res.json({ message: 'Ok' });
+	res.json({ message: 'Welcome to the Home Page!' });
 });
 
 
